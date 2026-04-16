7a6220e412a6ce653da12391c5c6114d50515a1f
APecherkin <pech.artyom2@yandex.ru>
2026-04-16 20:38:57 +0500

    RCA-06: Add colors

--- app/src/main/java/ru/example/recipecomposeapp/theme/Theme.kt
+++ app/src/main/java/ru/example/recipecomposeapp/theme/Theme.kt
@@ -10,26 +10,38 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val RecipesAppDarkColorScheme = darkColorScheme(
+    primary = PrimaryColorDark,
+    onPrimary = TextPrimaryColorDark,
+    error = AccentColorDark,
+    onError = TextSecondaryColorDark,
+    tertiary = AccentColorDark,
+    onTertiary = TextSecondaryColorDark,
+    tertiaryContainer = SliderTrackColorDark,
+    onTertiaryContainer = AccentBlueDark,
+    background = BackgroundColorDark,
+    surface = SurfaceColorDark,
+    onSurface = TextPrimaryColorDark,
+    surfaceVariant = SurfaceVariantColorDark,
+    onSurfaceVariant = TextSecondaryColorDark,
+    outline = DividerColorDark,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val RecipesAppLightColorScheme = lightColorScheme(
+    primary = PrimaryColor,
+    onPrimary = TextPrimaryColor,
+    error = AccentColor,
+    onError = TextSecondaryColor,
+    tertiary = AccentColor,
+    onTertiary = TextSecondaryColor,
+    tertiaryContainer = SliderTrackColor,
+    onTertiaryContainer = AccentBlue,
+    background = BackgroundColor,
+    surface = SurfaceColor,
+    onSurface = TextPrimaryColor,
+    surfaceVariant = SurfaceVariantColor,
+    onSurfaceVariant = TextSecondaryColor,
+    outline = DividerColor,
 )
 
 @Composable
@@ -44,8 +56,8 @@ fun RecipesAppTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> RecipesAppDarkColorScheme
+        else -> RecipesAppLightColorScheme
     }
 
     MaterialTheme(
