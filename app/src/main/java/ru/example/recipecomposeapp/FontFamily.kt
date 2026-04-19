da975759984852194ece2f8019088b779b4238cd
APecherkin <pech.artyom2@yandex.ru>
2026-04-19 12:54:48 +0500

    RCA-05: Исправлены шрифты

--- app/src/main/java/ru/example/recipecomposeapp/FontFamily.kt
+++ app/src/main/java/ru/example/recipecomposeapp/FontFamily.kt
@@ -2,11 +2,13 @@ package ru.example.recipecomposeapp
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 
 val montserratFontFamily = FontFamily(
-    Font(R.font.montserrat_medium)
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+    Font(R.font.montserrat_regular, FontWeight.Normal),
 )
 
 val montserratAlternatesFontFamily = FontFamily(
-    Font(R.font.montserrat_alternates_semi_bold)
+    Font(R.font.montserrat_alternates_semi_bold, FontWeight.SemiBold)
 )
