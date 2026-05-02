5c80da408946a7a652d4095004d60a51d04d5746
APecherkin <pech.artyom2@yandex.ru>
2026-05-02 16:36:43 +0500

    RCA-12

--- app/src/main/java/ru/example/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/ru/example/recipecomposeapp/RecipesApp.kt
@@ -1,25 +1,18 @@
 package ru.example.recipecomposeapp
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import ru.example.recipecomposeapp.navigation.BottomNavigation
 import ru.example.recipecomposeapp.theme.RecipesAppTheme
 import ru.example.recipecomposeapp.ui.categories.CategoriesScreen
-import ru.example.recipecomposeapp.core.ui.ScreenHeader
 import ru.example.recipecomposeapp.ui.favorites.FavoritesScreen
 
 @Composable
