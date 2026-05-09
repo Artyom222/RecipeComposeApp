f774eb5f01a838bb1f4aad4fbe059beda1502bcd
APecherkin <pech.artyom2@yandex.ru>
2026-05-09 17:44:59 +0500

    RCA-11

--- app/src/main/java/ru/example/recipecomposeapp/data/model/RecipeDto.kt
+++ app/src/main/java/ru/example/recipecomposeapp/data/model/RecipeDto.kt
@@ -3,7 +3,7 @@ package ru.example.recipecomposeapp.data.model
 data class RecipeDto(
     val id: Int,
     val title: String,
-    val ingredients: IngredientDto,
+    val ingredients: List<IngredientDto>,
     val method: List<String>,
     val imageUrl: String,
 )
