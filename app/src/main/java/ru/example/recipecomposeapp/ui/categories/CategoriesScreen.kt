f4d6fc385cd0730f2e9aecec7b6369506d1525e4
APecherkin <pech.artyom2@yandex.ru>
2026-05-24 21:19:40 +0500

    RCA-14

--- app/src/main/java/ru/example/recipecomposeapp/ui/categories/CategoriesScreen.kt
+++ app/src/main/java/ru/example/recipecomposeapp/ui/categories/CategoriesScreen.kt
@@ -1,18 +1,17 @@
 package ru.example.recipecomposeapp.ui.categories
 
-import android.graphics.BitmapFactory
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import ru.example.recipecomposeapp.R
 import ru.example.recipecomposeapp.core.ui.ScreenHeader
 import ru.example.recipecomposeapp.data.repository.getCategories
@@ -22,9 +21,9 @@ import ru.example.recipecomposeapp.ui.categories.model.toUiModel
 @Composable
 fun CategoriesScreen(
     modifier: Modifier = Modifier,
-    onCategoryClick: (Int) -> Unit
+    onCategoryClick: (Int, String) -> Unit
 ) {
-    Column(modifier = modifier) {
+    Column {
         ScreenHeader(
             imagePainter = painterResource(id = R.drawable.bcg_categories),
             contentDescription = "Категории",
@@ -32,14 +31,21 @@ fun CategoriesScreen(
         )
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
-            modifier = Modifier
-                .padding(Dimens.PaddingMedium)
+            contentPadding = PaddingValues( horizontal = Dimens.PaddingMain),
+            verticalArrangement = Arrangement.spacedBy(Dimens.PaddingMain),
+            horizontalArrangement = Arrangement.spacedBy(Dimens.PaddingMain),
+            modifier = modifier
+                .fillMaxSize()
         ) {
-            items(getCategories()) { category ->
+            items(
+                items = getCategories(),
+                key = { it.id }
+            ) { category ->
+                val uiModelCategory = category.toUiModel()
                 CategoryItem(
-                    category = category.toUiModel(),
-                    onClick = { onCategoryClick(category.id) },
-                    modifier = modifier,
+                    category = uiModelCategory,
+                    onClick = { onCategoryClick(uiModelCategory.id, uiModelCategory.title) },
+                    modifier = Modifier,
                 )
             }
         }
--- app/src/main/java/ru/example/recipecomposeapp/ui/categories/CategoryItem.kt
+++ app/src/main/java/ru/example/recipecomposeapp/ui/categories/CategoryItem.kt
@@ -1,13 +1,8 @@
 package ru.example.recipecomposeapp.ui.categories
 
-import android.content.res.Configuration
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -16,24 +11,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import coil3.request.ImageRequest
-import coil3.request.crossfade
 import ru.example.recipecomposeapp.R
 import ru.example.recipecomposeapp.data.model.CategoryDto
 import ru.example.recipecomposeapp.theme.Dimens
 import ru.example.recipecomposeapp.ui.categories.model.CategoryUiModel
 import ru.example.recipecomposeapp.ui.categories.model.toUiModel
-import java.util.Locale
-
 
 @Composable
 fun CategoryItem(
@@ -42,9 +30,8 @@ fun CategoryItem(
     modifier: Modifier = Modifier,
 ){
     Card(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .padding(Dimens.PaddingMedium)
             .clickable { onClick() },
         shape = RoundedCornerShape(Dimens.PaddingMedium),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
